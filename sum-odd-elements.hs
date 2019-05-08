3c3356b36f7679742fb515d8a1b9ec3a23c6145f
Antti <antti.hallavo@aalto.fi>
2019-05-08 23:12:21 +0300

    Returns the sum of odd elements in a list

---
+++ sum-odd-elements.hs
@@ -0,0 +1,7 @@
+f :: [Int] -> Int
+f arr = sum $ filter odd arr
+
+-- This part handles the Input/Output and can be used as it is. Do not change or modify it.
+main = do
+  inputdata <- getContents
+  putStrLn $ show $ f $ map (read :: String -> Int) $ lines inputdata
